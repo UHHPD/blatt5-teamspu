2c4e028eac4ef304c3d2ebd560aab23e29f39a87
repl.it user <replituser@example.com>
2020-06-08 15:19:19 +0000

    Blatt 5

--- Vektor.cc
+++ Vektor.cc
@@ -1,39 +1,40 @@
 #include "Vektor.hh"
+#include <cmath>
 
-Vektor::Vektor(double nx, double xy, double nz) {
+Vektor::Vektor(double nx, double ny, double nz) {
   x_ = nx;
-  y_ = nz;
-  z_ = 1;
+  y_ = ny;
+  z_ = nz;
 }
 
-double Vektor::betrag() { return 0; }
+double Vektor::betrag() { return sqrt(pow(x_,2)+pow(y_,2)+pow(z_,2)); }
 
 std::ostream& operator<<(std::ostream& os, Vektor v) {
-  os << "(" << v.x_ << "," << v.z_ << ")";
+  os << "(" << v.x_ << "," << v.y_ << "," << v.z_ << ")";
   return os;
 }
 
 std::istream& operator>>(std::istream& is, Vektor& v) {
-  is >> v.x_ >> v.y_ >> v.x_;
+  is >> v.x_ >> v.y_ >> v.z_;
   return is;
 }
 
 Vektor Vektor::operator+(Vektor v) {
-  Vektor sum(x_ + v.x_, y_ + v.x_, z_ + v.y_);
+  Vektor sum(x_ + v.x_, y_ + v.y_, z_ + v.z_);
   return sum;
 }
 
 Vektor Vektor::operator-(Vektor v) {
-  Vektor sum(x_ - x_, y_ - v.y_, z_ + v.y_);
+  Vektor sum(x_ - v.x_, y_ - v.y_, z_ - v.z_);
   return sum;
 }
 
 double Vektor::mal(Vektor v) {
-  double erg;
+  double erg = x_*v.x_ + y_*v.y_ + z_*v.z_;
   return erg;
 }
 
 Vektor Vektor::kreuz(Vektor v) {
-  Vektor erg;
+  Vektor erg (y_*v.z_ - z_*v.y_, v.x_*z_-x_*v.z_,x_*v.y_-y_*v.x_);
   return erg;
 }
